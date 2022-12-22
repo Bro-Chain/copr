b1256574be5fe6e71a484ee6f287812f4493f35c
mice <mice@brocha.in>
2022-12-22 21:50:53 +0100

    Took out some garbage

--- CosmosProposalBot/Modules/SubscribeModule.cs
+++ CosmosProposalBot/Modules/SubscribeModule.cs
@@ -15,50 +15,6 @@ namespace CosmosProposalBot.Modules;
 [Discord.Interactions.Group("subscribe","Subscribe to notifications")]
 public class SubscribeModule : InteractionModuleBase
 {
-    private readonly Dictionary<string, string> _proposalTypeMapping = new Dictionary<string, string>
-    {
-        {"/cosmos.distribution.v1beta1.CommunityPoolSpendProposal", "Software Upgrade"},
-        {"/cosmos.gov.v1beta1.TextProposal", "Text"},
-        {"/cosmos.params.v1beta1.ParameterChangeProposal", "Parameter Change"},
-        {"/cosmos.upgrade.v1beta1.SoftwareUpgradeProposal", "Software Upgrade"},
-        {"/cosmos.upgrade.v1beta1.CancelSoftwareUpgradeProposal", "Cancel Software Upgrade"},
-        {"/cosmos.upgrade.v1beta1.PlanChangeProposal", "Plan Change"},
-        {"/cosmos.upgrade.v1beta1.CancelPlanChangeProposal", "Cancel Plan Change"},
-
-        {"/cosmwasm.wasm.v1.StoreCodeProposal", "WASM Store Code"},
-
-        {"/ibc.core.client.v1.ClientUpdateProposal", "IBC Client Update"},
-
-        {"/acrechain.erc20.v1.RegisterCoinProposal", "Register Coin"},
-
-        {"/agoric.swingset.CoreEvalProposal", "Core Eval"},
-
-        {"/canto.govshuttle.v1.LendingMarketProposal", "Lending Market"},
-
-        {"/comdex.asset.v1beta1.AddAssetsProposal", "Add Asset"},
-        {"/comdex.bandoracle.v1beta1.FetchPriceProposal", "Fetch Price"},
-        
-        {"/Switcheo.carbon.liquiditypool.SetRewardCurveProposal", "Set Reward Curve"},
-        {"/Switcheo.carbon.liquiditypool.LinkPoolProposal", "Link Pool"},
-        {"/Switcheo.carbon.liquiditypool.UnlinkPoolProposal", "Unlink Pool"},
-        {"/Switcheo.carbon.liquiditypool.SetPoolFeeProposal", "Set Pool Fee"},
-        {"/Switcheo.carbon.liquiditypool.SetPoolFeeAddressProposal", "Set Pool Fee Address"},
-        {"/Switcheo.carbon.liquiditypool.SetPoolFeeRateProposal", "Set Pool Fee Rate"},
-        {"/Switcheo.carbon.liquiditypool.SetPoolFeeCapProposal", "Set Pool Fee Cap"},
-        {"/Switcheo.carbon.liquiditypool.SetPoolFeeCollectorProposal", "Set Pool Fee Collector"},
-        {"/Switcheo.carbon.liquiditypool.SetPoolFeeCollectorAddressProposal", "Set Pool Fee Collector Address"},
-        {"/Switcheo.carbon.liquiditypool.SetPoolFeeCollectorRateProposal", "Set Pool Fee Collector Rate"},
-        {"/Switcheo.carbon.liquiditypool.SetPoolFeeCollectorCapProposal", "Set Pool Fee Collector Cap"},
-        {"/Switcheo.carbon.liquiditypool.SetPoolFeeCollectorEnabledProposal", "Set Pool Fee Collector Enabled"},
-        {"/Switcheo.carbon.liquiditypool.SetPoolFeeCollectorDisabledProposal", "Set Pool Fee Collector Disabled"},
-
-        // {"1", "Software Upgrade"},
-        // {"1", "Software Upgrade"},
-        // {"1", "Software Upgrade"},
-        // {"1", "Software Upgrade"}
-    };
-
-        
     private readonly ILogger<SubscribeModule> _logger;
     private readonly IOptions<BotOptions> _options;
     private readonly IServiceProvider _serviceProvider;
